8938bf701ba6dbcbf85abc22157a11643b8bfc25
a0s01gm <abhinandan.sonvane@walmartlabs.com>
2020-03-14 18:03:25 +0530

    Added problems

--- src/main/java/com/as/leetcode/strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/as/leetcode/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,8 +1,6 @@
-package com.as.leetcode.problem;
+package com.as.leetcode.strings;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
---
+++ src/main/java/com/as/leetcode/trees/InorderTraversal.java
@@ -0,0 +1,82 @@
+package com.as.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class InorderTraversal {
+  public static List<Integer> inorderTraversal(TreeNode root) {
+
+    if (null == root) {
+      return Collections.emptyList();
+    }
+
+    List<Integer> leftList = new ArrayList<>();
+    if (null != root.left) {
+      leftList = inorderTraversal(root.left);
+    }
+
+    leftList.add(root.val);
+
+    List<Integer> rightList = new ArrayList<>();
+    if (null != root.right) {
+      rightList = inorderTraversal(root.right);
+    }
+
+    leftList.addAll(rightList);
+
+    return leftList;
+  }
+
+  public static List<Integer> inorderTraversalIterative(TreeNode root) {
+
+    if (null == root) {
+      return Collections.emptyList();
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    Stack<TreeNode> stack = new Stack<>();
+
+    TreeNode node = root;
+
+    while (stack.size() > 0 || node != null) {
+
+      while(node!=null){
+        stack.push(node.left);
+        node = node.left;
+      }
+
+      node = stack.pop();
+      list.add(node.val);
+      node = node.right;
+
+    }
+
+    return list;
+  }
+
+  public static void main(String[] args) {
+
+    TreeNode n1 = new TreeNode(1);
+    TreeNode n2 = new TreeNode(2);
+    TreeNode n3 = new TreeNode(3);
+
+    n1.left = null;
+    n1.right = n2;
+    n2.left = n3;
+
+    System.out.println(inorderTraversalIterative(n1));
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
